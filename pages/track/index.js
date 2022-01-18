82427cc1a24dc9b58af833864b17142197ba79ea
brandon <bdickson1212@gmail.com>
2022-01-18 11:17:14 +0100

    feat:users can use a custom telegram chat for alerts

--- pages/track/index.js
+++ pages/track/index.js
@@ -4,11 +4,13 @@ import Button from '../../components/form/button';
 import ContentWrapper from '../../components/contentWrapper';
 import { useMoralis } from 'react-moralis';
 import { useState } from 'react';
+import { FaInfoCircle } from 'react-icons/fa';
 
 const TrackWalletsPage = () => {
   const { Moralis } = useMoralis();
   const [address, setAddress] = useState('');
   const [name, setName] = useState('');
+  const [chatId, setChatId] = useState('');
   // const [telegram, setTelegram] = useState(false);
   // const [email, setEmail] = useState(false);
   // const [twitter, setTwitter] = useState(false);
@@ -21,6 +23,8 @@ const TrackWalletsPage = () => {
       setAddress(value);
     } else if (name === 'name') {
       setName(value);
+    } else if (name === 'chatId') {
+      setChatId(value);
     }
     // else if (name === 'telegram') {
     //   setTelegram(!telegram);
@@ -41,16 +45,21 @@ const TrackWalletsPage = () => {
       //   (alert) => alert !== false
       // );
 
-      const params = { address: address.toLowerCase(), name };
+      const params = { address: address.toLowerCase(), name, chatId };
       await Moralis.Cloud.run('watchAddress', params);
       setAddress('');
       setName('');
+      setChatId('');
     } catch (err) {
       throw new Error(err.message);
     }
   };
 
-  const disabled = !name || !address || (!address.length === 42 && 'disabled');
+  const disabled =
+    !name ||
+    !address ||
+    address.length !== 42 ||
+    (chatId.length !== 14 && 'disabled');
 
   return (
     <ContentWrapper>
@@ -70,6 +79,14 @@ const TrackWalletsPage = () => {
           handleChange={handleChange}
           minLength={1}
           value={name}
+        />
+        <Input
+          type="text"
+          name="chatId"
+          placeholder="Telegram Chat ID"
+          handleChange={handleChange}
+          minLength={14}
+          value={chatId}
         />
         {/* <div className="flex mt-3">
           <div className="mr-5 text-gray-400">
@@ -100,6 +117,10 @@ const TrackWalletsPage = () => {
             Twitter
           </div> */}
         {/* </div> */}
+        <button className="text-blue-500 w-fit">
+          <FaInfoCircle className="inline mr-2" />
+          Follow these instructions to get your Telegram chat ID
+        </button>
         <Button
           handleSubmit={handleSubmit}
           disabled={disabled}
