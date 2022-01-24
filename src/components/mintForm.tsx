76884f7e6af7d4937332aece8e784ead11ee0ab8
Nelson Fleig <nfleig@hotmail.com>
2022-01-24 15:18:56 +0100

    refactor: added type and lint checking as well typescript refactoring

--- src/components/mintForm.tsx
+++ src/components/mintForm.tsx
@@ -1,17 +1,30 @@
+import { changeResponse, changeTxStatus } from '@/state/actions';
+import { useAppSelector } from '@/state/hooks';
 import React, { ChangeEventHandler, useState } from 'react';
-import SubmitButton from './form/submit-button';
+import { MoralisFileSaveOptions, useMoralis } from 'react-moralis';
+import { useDispatch } from 'react-redux';
 import Form from './form/form';
 import Input from './form/input';
+import SubmitButton from './form/submit-button';
+import Modal from './modal';
+import TxStatus from './txStatus';
 
-interface MintFormProps {
-  title: string;
-  handleSubmit: React.FormEventHandler<HTMLFormElement>;
-}
+const MintForm = () => {
+  const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+  const { Moralis, account } = useMoralis();
 
-const MintForm = ({ title, handleSubmit }: MintFormProps) => {
   const [nameInput, setNameInput] = useState('');
   const [descriptionInput, setDescriptionInput] = useState('');
-  const [file, setFile] = useState('');
+  const [file, setFile] = useState<HTMLInputElement | File>();
+
+  const { response } = useAppSelector(state => state);
+
+  const message = 'NFT Successfully Minted';
+  const res = (response as any).data && (response as any).data.result;
+  const link =
+    res && `https://rinkeby.rarible.com/token/flow/${res.tokenAddress}:${res.tokenId}?tab=details`;
+  const linkText = 'View on Rarible';
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = e => {
     const elName = e.target.name;
@@ -21,33 +34,91 @@ const MintForm = ({ title, handleSubmit }: MintFormProps) => {
       setNameInput(value);
     } else if (elName === 'description') {
       setDescriptionInput(value);
-    } else if (elName === 'upload') {
+    } else if (elName === 'upload' && e.target.files && e.target.files.length) {
       setFile(e.target.files[0]);
+    }
+  };
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async e => {
+    e.preventDefault();
+    setShowModal(true);
+    dispatch(changeTxStatus('pending'));
+
+    if (!nameInput || !descriptionInput || !file) return;
+
+    try {
+      await (Moralis as any).enableWeb3();
+      // Upload image to IPFS
+
+      type MoralisFile = typeof Moralis.File & {
+        hash: () => string;
+        saveIPFS: (options?: MoralisFileSaveOptions & { useMasterKey: boolean }) => Promise<File>;
+      };
+
+      const imageFile = new Moralis.File(file.name, file) as unknown as MoralisFile;
+
+      await imageFile.saveIPFS({ useMasterKey: true });
+      const hash = imageFile.hash();
+
+      // Create metadata
+      const metadata = {
+        name: nameInput,
+        description: descriptionInput,
+        image: `/ipfs/${hash}`,
+      };
+
+      // Upload metadate to IPFS
+      const jsonFile = new Moralis.File('metadata.json', {
+        base64: btoa(JSON.stringify(metadata)),
+      }) as unknown as MoralisFile;
+
+      await jsonFile.saveIPFS();
+      const metadataHash = jsonFile.hash();
+
+      // Upload to Rarible
+      await Moralis.Plugins.rarible
+        .lazyMint({
+          chain: 'rinkeby',
+          userAddress: account,
+          tokenType: 'ERC721',
+          tokenUri: `ipfs/${metadataHash}`,
+          royaltiesAmount: 0,
+        })
+        .then((raribleRes: string) => {
+          dispatch(changeResponse(raribleRes));
+          dispatch(changeTxStatus('success'));
+        });
+    } catch (err) {
+      if (err instanceof Error) {
+        dispatch(changeResponse(err.message));
+        dispatch(changeTxStatus('error'));
+      }
     }
   };
 
   const disabled = !nameInput || !descriptionInput || !file;
 
   return (
-    <Form
-      handleSubmit={e => handleSubmit(e, file, nameInput, descriptionInput)}
-      title={title}
-      titleColour="text-white">
-      <Input name="name" placeholder="NFT Name" handleChange={handleChange} minLength={1} />
-      <Input
-        name="description"
-        placeholder="NFT Description"
-        handleChange={handleChange}
-        minLength={1}
-      />
-      <Input name="upload" type="file" handleChange={handleChange} />
+    <>
+      <Form handleSubmit={handleSubmit} title="LazyMint NFT on Rarible" titleColour="text-white">
+        <Input name="name" placeholder="NFT Name" handleChange={handleChange} minLength={1} />
+        <Input
+          name="description"
+          placeholder="NFT Description"
+          handleChange={handleChange}
+          minLength={1}
+        />
+        <Input name="upload" type="file" handleChange={handleChange} />
 
-      <SubmitButton
-        handleSubmit={e => handleSubmit(e, file, nameInput, descriptionInput)}
-        disabled={disabled}
-        action="Mint"
-      />
-    </Form>
+        <SubmitButton disabled={disabled} action="Mint" />
+      </Form>
+
+      <Modal show={showModal} cancel={() => setShowModal(false)}>
+        <TxStatus message={message} link={link} linkText={linkText}>
+          <></>
+        </TxStatus>
+      </Modal>
+    </>
   );
 };
 
